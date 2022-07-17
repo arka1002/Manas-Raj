dd61b62fee5215bdb0744c5372d8d01830618674
arka1002 <arka.mcu700@gmail.com>
2022-07-17 17:40:23 +0530

    final haul

--- code.js
+++ code.js
@@ -146,7 +146,7 @@ function ValidateEmail(inputText)
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(inputText.value.match(mailformat))
 {
-alert("Valid email address!");
+// alert("Valid email address!");
 document.form1.email.focus();
 return true;
 }
